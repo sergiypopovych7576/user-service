6fc0eec87759a57d09728adb019f574f90727b41
Sergiy Popovych <popovychsergiy76@gmail.com>
2021-04-03 10:37:03 +0300

    Initial Commit

---
+++ User.Service/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace User.Service.Controllers
+{
+    [Route("user")]
+    public class UserController : BaseController
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "UP";
+        }
+    }
+}
